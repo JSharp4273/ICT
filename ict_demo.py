96c48d686f00f1097843f0b9d4dda4c3a4174dd8
Julien Fleuret <julien.fleuret@gmail.com>
2019-10-08 15:51:56 -0400

    almost final version

--- ict_demo.py
+++ ict_demo.py
@@ -8,7 +8,11 @@ Created on Wed Oct  2 15:40:25 2019
 
 import numpy as np, h5py as h5, os, cv2
 from scipy.io import loadmat
+from scipy.stats import iqr
+from scipy.signal import medfilt2d
 from skimage.morphology import remove_small_holes, remove_small_objects
+from skimage.filters import threshold_otsu
+from skimage.exposure import rescale_intensity
 from sklearn.decomposition import FastICA, PCA
 from matplotlib import pyplot as plt
 
@@ -113,8 +117,8 @@ def openmat(filename, field = None):
     @brief Open a .mat file and from its content return it all (field == -1) or
     return the specified field or fields as specified to the variable field
     
-    @param filename : Name of the file to be open.
-    @param fields : Name of the field, or fields to be loaded. 
+    @param filename  Name of the file to be open.
+    @param fields Name of the field, or fields to be loaded. 
     If fields is set to -1 all the field content in the file will be load.
     """
     tmp = []
@@ -158,18 +162,18 @@ def openmat(filename, field = None):
 
 def fromSequenceToOverlay(sequence, frames_first = False, apply_standardization = True):
     """ fromSequenceToOverlay(sequence [, frame_first [, apply_normalization] ]) -> overlay, dims
-    This function return the overlay matrix of a sequence of images.
+    @brief This function return the overlay matrix of a sequence of images.
     The overlay is the a matrix in which each column is the flatten
     representation of an image of the input sequence.
     
-    @param sequence : It can be a 3d ndarray, a list or a tuple of ndarray.
-    @param frames_first : If true it means that the parameter sequence 
+    @param sequence It can be a 3d ndarray, a list or a tuple of ndarray.
+    @param frames_first If true it means that the parameter sequence 
     dimensions are organize in way that the number of frames is represented by
     the first dimension.
     Oftenly this organization shape is: frames, rows, cols
     If frame_first is False the shape of the data is supposed to be organized
     like this: rows, cols, frames.
-    @param apply_normalization : If true the overlay will be centred and
+    @param apply_normalization If true the overlay will be centred and
     reduced by its variance.    
     """    
     sequence = np.asarray(sequence)
@@ -197,13 +201,14 @@ def fromSequenceToOverlay(sequence, frames_first = False, apply_standardization 
 def fromOverlayToSequence(overlay, dims, frames_first = False):
     
     """ fromOverlayToSequence(overlay, dims [, frames_first])->sequence
-    @param overlay : a 2D matrix (ndarray). Each column correspond to an image.
-    @param dims : a list, a tuple or a ndarray containing the number of rows,
+    @brief  This function return a sequence of frames from a overlay matrix.
+    @param overlay a 2D matrix (ndarray). Each column correspond to an image.
+    @param dims a list, a tuple or a ndarray containing the number of rows,
     cols and frames.
     If dims is set to None it means that the overlay must be return as is.
     @param frames_first : should the returned sequence be organize with the 
     frames as first dimension ? (default: False)
-    @param sequence : a 3D matrix (ndarray) 
+    @param sequence a 3D matrix (ndarray) 
     """
     
     assert isinstance(dims,(list, tuple, np.ndarray)) or dims is None
@@ -225,23 +230,23 @@ def fromOverlayToSequence(overlay, dims, frames_first = False):
 
 def ApplyNCT(method, sequence, apply_standardization, frames_first, **kwargs):
     """ ApplyNCT(method, sequence, apply_normalization, frames_first, **kwargs)-> method_argouts
-    This function apply a sequence 
+    @brief This function apply a sequence 
     
-    @param method : function or object that will be used to process the data.
+    @param method function or object that will be used to process the data.
     
-    @param sequence : either a overlay matrix, a three dimensional array, or a tuple or
+    @param sequence either a overlay matrix, a three dimensional array, or a tuple or
     a list of matrix.
     
-    @param apply_standardization : boolean value to set in order to apply a standardization or not
+    @param apply_standardization boolean value to set in order to apply a standardization or not
     
-    @param frame_first : If true it means that the parameter sequence 
+    @param frame_first If true it means that the parameter sequence 
     dimensions are organize in way that the number of frames is represented by
     the first dimension.
     Oftenly this organization shape is: frames, rows, cols
     If frame_first is False the shape of the data is supposed to be organized
     like this: rows, cols, frames.
 
-    @param method_argouts : output argmuments resulting from the processing of the variable sequence by the variable assign in method
+    @param method_argouts output argmuments resulting from the processing of the variable sequence by the variable assign in method
     
     """
     sequence = np.asarray(sequence)
@@ -267,26 +272,26 @@ def ApplyNCT(method, sequence, apply_standardization, frames_first, **kwargs):
 def PCT(sequence, n_components = None, apply_standardization = True, frames_first = False):
     """PCT(sequence [,n_components [, apply_normalization [, frames_first] ] ])
     -> sequence_pct
-    This function implement the Principal Component Thermograpy as describe by
+    @brief This function implement the Principal Component Thermograpy as describe by
     Rajic.
     
-    @param sequence : either a overlay matrix, a three dimensional array, or a tuple or
+    @param sequence either a overlay matrix, a three dimensional array, or a tuple or
     a list of matrix.
     
-    @param n_components : the number of component to keep. If not set the 
+    @param n_components the number of component to keep. If not set the 
     sequence returned as the same size a the input sequence.
     
-    @param apply_normalization : if set the overlay matrix on which the PCT is
+    @param apply_normalization if set the overlay matrix on which the PCT is
     processed will first be centred around its mean and reduce by its variance.
     
-    @param frame_first : If true it means that the parameter sequence 
+    @param frame_first If true it means that the parameter sequence 
     dimensions are organize in way that the number of frames is represented by
     the first dimension.
     Oftenly this organization shape is: frames, rows, cols
     If frame_first is False the shape of the data is supposed to be organized
     like this: rows, cols, frames.
     
-    @param sequence_pct : sequence reconstructed after application of PCT.
+    @param sequence_pct Sequence reconstructed after application of PCT.
     """
 
     return ApplyNCT(PCA, sequence, frames_first=frames_first, \
@@ -296,26 +301,26 @@ def PCT(sequence, n_components = None, apply_standardization = True, frames_firs
 def ICT(sequence, n_components = None, apply_standardization = True, frames_first = False, **kwargs):
     """PCT(sequence [,n_components [, apply_normalization [, frames_first
     [, **kwargs] ] ] ]) -> sequence_pct
-    This function implement the Independant Component Thermograpy as describe
+    @brief This function implement the Independant Component Thermograpy as describe
     by no one yet.
     
-    @param sequence : either a overlay matrix, a three dimensional array, or a tuple or
+    @param sequence either a overlay matrix, a three dimensional array, or a tuple or
     a list of matrix.
     
-    @param n_components : the number of component to keep. If not set the 
+    @param n_components the number of component to keep. If not set the 
     sequence returned as the same size a the input sequence.
     
     @param apply_normalization : if set the overlay matrix on which the PCT is
     processed will first be centred around its mean and reduce by its variance.
     
-    @param frame_first : If true it means that the parameter sequence 
+    @param frame_first If true it means that the parameter sequence 
     dimensions are organize in way that the number of frames is represented by
     the first dimension.
     Oftenly this organization shape is: frames, rows, cols
     If frame_first is False the shape of the data is supposed to be organized
     like this: rows, cols, frames.
     
-    @param kwargs : this code use the function FastICA from scikit-learn.
+    @param kwargs this code use the function FastICA from scikit-learn.
     Kwargs allow to send some extra configuration parameters to the function
     FastICA.
     
@@ -328,18 +333,18 @@ def ICT(sequence, n_components = None, apply_standardization = True, frames_firs
 
 def get_roi(I, frames_first=False):
     """get_roi(I [, frames_first]) -> roi
-    This function look for a rectangular region of interest in an image and return its bounding box parameters.
+    @brief This function look for a rectangular region of interest in an image and return its bounding box parameters.
     
-    @param I : source image or sequence.
+    @param I source image or sequence.
     
-    @param frame_first : If true it means that the parameter I is a sequence from which
+    @param frame_first If true it means that the parameter I is a sequence from which
     dimensions are organize in way that the number of frames is represented by
     the first dimension.
     Oftenly this organization shape is: frames, rows, cols.
     If frame_first is False the shape of the data is supposed to be organized
     like this: rows, cols, frames.
     
-    @param roi : main parameters of the roi found in the image.
+    @param roi main parameters of the roi found in the image.
     i.e. coordinates (x,y) and size (width, height)
     
     """
@@ -426,33 +431,152 @@ def get_roi(I, frames_first=False):
     
     return x, y, w, h
 
+class JensenShanon_:
+    
+    def __init__(self):
+#        components_ : convinient variable name which allow to use ApplyNCT algorithm.
+        self.entropy = None
+        self.cst = np.log(2)
+
+    def _computeEntropy(self, P, Q):
+        
+        PP, QQ = P.copy(), Q.copy()
+        PP[PP<1] = 1
+        QQ[QQ<1] = 1
+        
+        PQ = P+Q
+        PQ[PQ<1] = 1
+        
+        left = P * ( (self.cst + np.log(PP) ) - np.log(PQ) )
+        right = Q * ( (self.cst + np.log(QQ) ) - np.log(PQ) )
+        
+        return 0.5 * (np.sum(left) + np.sum(right) )
+
+    def fit(self, sequence):
+        
+        sequence = np.asarray(sequence)
+        
+        frames, _ = sequence.shape
+        
+        self.entropy = np.zeros((frames,), np.float32)    
+        
+#        tmp = iqr(sequence, axis=0)        
+#        
+#        self.entropy[0] = self._computeEntropy(tmp, sequence[0])
+        
+        for f in range(frames-2):
+            I_current = sequence[f]
+            I_next = sequence[f+1]
+            
+            self.entropy[f+1] = self._computeEntropy(I_current, I_next)
+        
+        return self
+        
+def computeJensenShanon(sequence, frames_first):
+    """computeJensenShanon(sequence, frames_first)->entropy
+    @brief This function compute the divergence using the Jensen Shanon formulation.
+    
+    @param sequence either a overlay matrix, a three dimensional array, or a tuple or
+    a list of matrix.
+    
+    @param frame_first If true it means that the parameter I is a sequence from which
+    dimensions are organize in way that the number of frames is represented by
+    the first dimension.
+    Oftenly this organization shape is: frames, rows, cols.
+    If frame_first is False the shape of the data is supposed to be organized
+    like this: rows, cols, frames.    
+
+    @return entropy a vector of the same length of the sequence,
+    which contains the interframe entropy of the images of the input sequence.
+    Note in order to have the same number of elements than the number image,
+    the first entropy is computed between the inter-quantile range of the sequence
+    and the first frame.
+    
+    
+    """
+    
+    sequence = np.asarray(sequence)
+    
+    assert sequence.ndim == 3 or sequence.ndim == 2
+    
+    
+    JS = JensenShanon_()
+    
+    overlay, _ = fromSequenceToOverlay(sequence,
+                         frames_first=frames_first,
+                         apply_standardization=True)
+            
+    return JS.fit(overlay).entropy
+    
+    
+
 if __name__ == '__main__':
-    
-    fn1 = '/media/smile/New Volume/julien/materials/cfrp_006_front.mat'
-    fn2 = '/home/smile/octave_dir/joint_experiment_lei_lei/CFRP_Ctrl.mat'
-    
-    data = openmat(fn2)
-#    data = data['a65']
+        
+    data = openmat('./data/CFRP_006_compressed.mat')
+    if isinstance(data, dict):
+        data = data['a65']
+    else:
+#       0, 1, 2 -> FLIR a65, FLIR phoenix, JenOptik Variocam HD
+        data = data[0]
+
+    use_automatic_finding = False    
+
     
     if isinstance(data, list):
         data = data[0]
         
         data = data.transpose((2,0,1))
     
-#    data = np.asarray([d.T for d in data])
+    I8 = data[0].T
+    mu, sigma = cv2.meanStdDev(I8)
+    I8 = (I8 - mu) / sigma
+#       The median filter is used in order to remove the dead pixels.
+    I8 = medfilt2d(I8) 
+    I8 = rescale_intensity(I8, out_range=(0,255)).astype(np.uint8)
+
+    if not use_automatic_finding:        
+        x,y,w,h = cv2.selectROI(windowName='please select the region of interest.', img=I8)        
+        cv2.destroyWindow('please select the region of interest.')
+    else:
+        x,y,w,h = get_roi(I8)
+
+    del I8    
+#   Because the image has been transposed x,y,w,h must be reassign    
+    x,y,w,h = y,x,h,w    
     
-    x,y,w,h = get_roi(data[0])
+#   Compute the entropy, the moment where the flash have been activate correspond to the moment with the maximum entropy.    
+    entropy = computeJensenShanon(data[:, y:y+h, x:x+w], frames_first=True)
     
-    data = data[:400, y:y+h, x:x+w]
-    
+    plt.figure()
+    plt.plot(entropy)
+
+#   From the entopy determine the moment where the flash have been activate, compute the cool image and reduce the dataset.    
+    argmax = entropy.argmax()
+
+    cool_image = np.mean(data[:argmax, y:y+h, x:x+w],axis=0)
+            
+    data = data[argmax:argmax+200, y:y+h, x:x+w] - cool_image
+
+#   Compute both the ICT, and PCT, keep only the last 7 components.    
     data_ict = ICT(data, n_components=7, frames_first=True)
     data_pct = PCT(data, n_components=7, frames_first=True)
-
     
     for (I_ict, I_pct) in zip(data_ict, data_pct):
+        
+#       Apply a standization in order to ease the detection of the defects.        
+        (mu_i, sigma_i), (mu_p, sigma_p) = cv2.meanStdDev(I_ict), cv2.meanStdDev(I_pct)
+        
+#       The abolute value is used in order not to care about the gradient inversion.
+        I_ict, I_pct = np.abs( (I_ict - mu_i) / sigma_i), np.abs( (I_pct - mu_p) / sigma_p)
+        
+        I_ict, I_pct = I_ict >= threshold_otsu(I_ict), I_pct >= threshold_otsu(I_pct)
+        
+        I_ict, I_pct = remove_small_holes(remove_small_objects(I_ict)), remove_small_holes(remove_small_objects(I_pct))
+        
         plt.figure()
         plt.subplot(121)
         plt.imshow(I_ict, cmap='gray')
         plt.subplot(122)
         plt.imshow(I_pct, cmap='gray')
-
+        plt.show()
+        
